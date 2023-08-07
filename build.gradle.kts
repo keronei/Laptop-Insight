5d1125d28be8448fdade5b629b83a0b746e1b6ed
Keronei <lincolnkeronei95@gmail.com>
2023-08-07 19:56:20 +0300

    upgrade and unify Kotlin and AGP versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.0.4")
+        classpath ("com.android.tools.build:gradle:8.1.0")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath(BuildPlugins.navigationSafeArgs)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
