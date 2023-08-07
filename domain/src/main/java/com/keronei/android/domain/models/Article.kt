3b3181460b55f366e57a4af3f669ec59e4cb7154
Keronei <lincolnkeronei95@gmail.com>
2023-08-07 19:51:48 +0300

    Let article data class implement parcelable

--- domain/src/main/java/com/keronei/android/domain/models/Article.kt
+++ domain/src/main/java/com/keronei/android/domain/models/Article.kt
@@ -1,13 +1,16 @@
 package com.keronei.android.domain.models
 
-import java.util.*
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.Date
 
+@Parcelize
 data class Article(
-    val id : Int = 0,
+    val id: Int = 0,
     val title: String,
     val link: String,
     val description: String,
     val banner: String,
     val guid: String,
     val publicationDate: Date
-)
+) : Parcelable
